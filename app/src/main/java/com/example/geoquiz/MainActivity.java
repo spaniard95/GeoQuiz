f282ba1e3bc7f651814c4f3a7cb1aa36c9f90488
spaniard95 <panagiotis.spanos95@gmail.com>
2020-03-05 23:29:46 +0200

    fist chapter

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button mTrueButton,mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              // Does nothing yet, but soon!
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet, but soon!
+            }
+        });
     }
 }
